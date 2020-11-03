072d5f1191649e47f586cf7a531350b447563631
Luiz Fernando de Oliveira Martins <luizfernando_martins@hotmail.com>
2020-11-03 18:52:04 -0400

    Finalização Exibição dos dados

--- fetch-api/src/js/main.js
+++ fetch-api/src/js/main.js
@@ -1,17 +1,31 @@
 let cep = document.querySelector('#cep')
 
-cep.addEventListener('blur', (e)=>{
-    let search =cep.value.replace('-','')
 
-    var options={
-        method:'GET',
+let showData = (result) => {
+    for (const campo in result) {
+        if (document.querySelector('#' + campo)) {
+            document.querySelector('#' + campo).value = result[campo]
+
+
+        }
+    }
+}
+
+
+cep.addEventListener('blur', (e) => {
+    let search = cep.value.replace('-', '')
+
+    var options = {
+        method: 'GET',
         mode: 'cors',
         cache: 'default'
     }
-fetch(`https://viacep.com.br/ws/${search}/json`, options)
-.then(response=>{response.json()
-    .then(data => console.log(data))})
-.catch(e=> console.log('Deu erro Mensagem:'+e.message))
+    fetch(`https://viacep.com.br/ws/${search}/json`, options)
+        .then(response => {
+            response.json()
+            .then(data => showData(data))
+        })
+        .catch(e => console.log('Deu erro Mensagem:' + e.message))
 
-console.log(search)
+    console.log(search)
 })
